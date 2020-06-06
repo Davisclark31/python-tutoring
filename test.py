be4ee17fa07cd47dc0bab1d228088fef7879091e
Davis Clark <CreeperFire@Daviss-Air.hitronhub.home>
2020-06-06 16:07:32 -0700

    finding product

--- test.py
+++ test.py
@@ -2,26 +2,22 @@ from selenium import webdriver
 
 
 category = str(input("Enter category: "))
+keyword = str(input("Enter keyword: "))
+color = str(input("Enter color: "))
+
 driver = webdriver.Chrome("./chromedriver")
 driver.get("https://www.supremenewyork.com/shop/all/"+str(category))
 elems = driver.find_elements_by_class_name("inner-article")
 
-#for i in elems:
-#    print(i.find_elements_by_tag_name("a"))
-
-#driver.close()
-
+links = []
 for elem in elems:
-    if "Eyelet" in elem.find_element_by_class_name("product-name") in elem.find_element_by_class_name("name-link") and "Red" in elem.find_element_by_class_name("product-style") in elem.find_element_by_class_name("name-link"):
-        print(elem.get_attribute("href"))
+    product_name = elem.find_element_by_class_name("product-name")
+    product_name_link_text = product_name.find_element_by_class_name("name-link").text.lower()
+    if keyword.lower() in product_name_link_text:
+        product_style = elem.find_element_by_class_name("product-style")
+        product_style_link_text = product_style.find_element_by_class_name("name-link").text.lower()
+        if color.lower() in product_style_link_text:
+            links.append(product_style.find_element_by_class_name("name-link").get_attribute("href"))
 
-
-#       driver.get(elem.get_attribute("href"))
-#    driver.get("https://www.supremenewyork.com/shop/all/sweatshirts")
-
-#1: go to specified page (category)
-#2: get all links on page
-#3: filter only category links
-#4: filter to specified product (product-name)
-# #5: filter to specified colour (product-style)
-#6: open final list of links
+for link in links:
+    driver.get(link)
